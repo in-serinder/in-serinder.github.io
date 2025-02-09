f2a3c9798dda6fc6e0d00df1df4c941415bee624
alterhan565 <serinder@163.com>
2025-02-10 03:29:42 +0800

    Dumb

---
+++ README.md
@@ -0,0 +1,5 @@
+## This is my first personal website
+
+The website Building ....
+
+### This website is currently just a practice project for me. I plan to completely redesign and rebuild it in the future
---
+++ main.js
@@ -0,0 +1,66 @@
+var images = [
+    "124907858_p0.jpg",
+    "124907858_p1.jpg",
+    "124907858_p2.jpg",
+    "124907858_p3.jpg",
+]
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    NProgress.start();
+ 
+    // any
+    // if (document.readyState == 'complete') {
+    //     NProgress.done();
+    //     console.log("Page loaded");
+    // }
+
+    
+    var image=document.getElementById('image');
+
+    window.onload=function(){
+        SelectImage();
+        // GetDate();
+        // GetGMTDate();
+        NProgress.done();
+        
+        setInterval(GetGMTDate, 1000);
+    }
+});
+
+ 
+
+    
+    function SelectImage(){
+        var random=Math.floor(Math.random() * images.length);
+        image.src="image/"+images[random];
+    }
+
+    function GetDate(){
+       const date=new Date();
+       const year=date.getFullYear();
+       const month=date.getMonth()+1;
+       const day=date.getDate();
+
+    document.getElementById('date').innerHTML="But Now: "+year+"-"+month+"-"+day
+       
+    }
+
+    function GetGMTDate() {
+        const date = new Date();
+        const gmtDate = date.toUTCString(); 
+    
+        document.getElementById('gmt-date').innerHTML ="But Now :  "+ gmtDate; 
+    }
+
+
+
+   
+
+
+
+  // Or with jQuery
+
+
+
